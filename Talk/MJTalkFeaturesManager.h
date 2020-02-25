ea53116f298b8535d31d95158535e24b19d310ca
Mohammad <piade@live.com>
2020-02-25 21:04:49 +0330

    change appicon to TALK Icon

---
+++ Talk/MJTalkFeaturesManager.h
@@ -0,0 +1,65 @@
+//
+//  MJTalkFeaturesManager.h
+//  Telegraph
+//
+//  Created by rain on 2/25/20.
+//
+
+#ifndef MJTalkFeaturesManager_h
+#define MJTalkFeaturesManager_h
+
+//Change Icon
+//Change Name
+//Default Intro Images by disable all intro
+//change notification banner icon
+
+#define DisableSecretChat
+//from create and profile
+
+#define DisableCreateChannel
+
+//From Setting and start from first time
+#define DisableBlockUserFromUserInfo
+
+#define DisableUserMention
+
+#define DisableSearchInConversation
+//From inside conversation
+
+#define DisableHashtag
+//cant click hashtag
+
+#define DisableBot
+
+#define PinConversation
+
+#define DisableIntro
+
+#define DisableGroupAdminsFetures
+
+#define DisablePinMessages
+
+#define DisableSelfDestructiveInSendMedia
+
+#define DisableSendLocation
+
+#define DisableMultiMediaGroup
+#define DisableSelfVideoCapture
+#define Disable
+
+//Settings
+#define DisableAskQuestionAndFAQ
+#define DisableChangeLanguage
+#define DisableProxy
+#define DisableBlockedUsers
+#define DisableTwoStepVerification
+#define DisableDeleteMyAccount
+#define DisablePaymentSection
+#define DisableTrensStickers
+#define DisableStickerBot
+
+
+//TemprerlyDisableCreateGroupToFixIt
+
+
+#endif /* MJTalkFeaturesManager_h */
